7bad2b972babacc0561529e46e7fa474964b2e32
mdpakhmurin <mdpakhmurin@gmail.com>
2022-02-06 19:31:19 +0700

    Нарисован первый треугольник (По инструкции OpenTK)

--- Program.cs
+++ Program.cs
@@ -4,14 +4,73 @@ using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using OpenTK.Graphics.OpenGL4;
+using System.IO;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace Render
 {
     public class Window : GameWindow
     {
-        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : 
+        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) :
             base(gameWindowSettings, nativeWindowSettings)
         {
+        }
+
+        private float[] _vertices =
+        {
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+             0.0f,  0.5f, 0.0f
+        };
+
+        private int _vertexBufferObject;
+
+        private int _vertexArrayObject;
+
+        private int _program;
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            var vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, File.ReadAllText(@"Resources\Shaders\VertexShader.glsl"));
+            GL.CompileShader(vertexShader);
+
+            var fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, File.ReadAllText(@"Resources\Shaders\FragmentShader.glsl"));
+            GL.CompileShader(fragmentShader);
+
+            _program = GL.CreateProgram();
+            GL.AttachShader(_program, vertexShader);
+            GL.AttachShader(_program, fragmentShader);
+            GL.LinkProgram(_program);
+
+            GL.UseProgram(_program);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.UseProgram(_program);
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            SwapBuffers();
         }
     }
     class MainClass
