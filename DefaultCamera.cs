bb3ae59f3c78f85dabe9bf840c3ae8806a75f8a7
mdpakhmurin <mdpakhmurin@gmail.com>
2022-02-06 11:43:50 +0700

    Добавлен класс камеры

---
+++ DefaultCamera.cs
@@ -0,0 +1,16 @@
+﻿using GlmNet;
+using Scene.SceneObject;
+
+namespace Scene.Defaults
+{
+    class Camera: SceneObject.SceneObject
+    {
+        public Camera(string name = "newCamera") : base(name) { }
+
+        public float DistanceToNearPlane { get; set; }
+        public float DistanceToFarPlane { get; set; }
+
+        public vec2 AspectRatioNearPlane  { get; set; }
+        public vec2 AspectRatioFarPlane { get; set; }
+    }
+}
