c2f97dc48d63ee76d2232255a30216b4132d76f3
mdpakhmurin <mdpakhmurin@gmail.com>
2022-02-08 11:44:38 +0700

    Создание классов объектов, которые могут быть отрисованы

---
+++ DrawableObject.cs
@@ -0,0 +1,67 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.IO;
+using Scene.Utilities;
+
+namespace Scene.Defaults
+{
+    public class DrawableObject
+    {
+        virtual public void Draw(Camera camera) { }
+    }
+
+    public class VoxelGrid : DrawableObject
+    {
+        private ShaderProgram shaderProgram;
+        private int vao;
+
+        public VoxelGrid()
+        {
+            float[] canvasVerticesPos =
+            {
+                -0.9f, -0.9f, 0,
+                 0.9f, -0.9f, 0,
+                 0.9f,  0.9f, 0,
+                 0.9f,  0.9f, 0,
+                -0.9f,  0.9f, 0,
+                -0.9f, -0.9f, 0
+            };
+
+            int vbo = GL.GenBuffer();
+            vao = GL.GenVertexArray();
+
+
+            GL.BindVertexArray(vao);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer,
+                canvasVerticesPos.Length * sizeof(float),
+                canvasVerticesPos,
+                BufferUsageHint.StaticDraw);
+
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.BindVertexArray(0);
+
+            var vertexShader = new VertexShader(File.ReadAllText(@"Resources\Shaders\VertexShader.glsl"));
+            var fragmentShader = new FragmentShader(File.ReadAllText(@"Resources\Shaders\FragmentShader.glsl"));
+
+            shaderProgram = new ShaderProgram(vertexShader, fragmentShader);
+        }
+
+        override public void Draw(Camera camera) { 
+            base.Draw(camera);
+
+            shaderProgram.Use();
+
+            GL.BindVertexArray(vao);
+            GL.EnableVertexAttribArray(0);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+            GL.DisableVertexAttribArray(0);
+            GL.BindVertexArray(0);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,146 +1,28 @@
-﻿using System;
-using Scene.SceneObject;
-using OpenTK;
-using OpenTK.Graphics;
+﻿using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
-using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.Common;
-using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Desktop;
+using System;
 using System.IO;
-using OpenTK.Windowing.GraphicsLibraryFramework;
+using Scene.Utilities;
+using Scene.Defaults;
 
 namespace Render
 {
-    public abstract class Shader
-    {
-        protected int id;
-        public Shader(string shaderSourceCode) { }
-
-        public int Id
-        {
-            get { return id; }
-        }
-
-        protected static int compileShader(ShaderType shaderType, string shaderSourceCode)
-        {
-            var shader = GL.CreateShader(shaderType);
-            GL.ShaderSource(shader, shaderSourceCode);
-            GL.CompileShader(shader);
-
-            var shaderCompileLog = GL.GetShaderInfoLog(shader);
-            if (shaderCompileLog != "")
-                throw new Exception("Ошибка компиляции шейдера " + shaderCompileLog);
-
-            return shader;
-        }
-    }
-
-    public class VertexShader : Shader
-    {
-        public VertexShader(string shaderSourceCode) : base(shaderSourceCode)
-        {
-            id = compileShader(ShaderType.VertexShader, shaderSourceCode);
-        }
-    }
-
-    public class ShaderProgram
-    {
-        private int id;
-
-        private VertexShader vertexShader;
-        private FragmentShader fragmentShader;
-        public ShaderProgram(VertexShader vertexShader, FragmentShader fragmentShader)
-        {
-            id = GL.CreateProgram();
-
-            VertexShader = vertexShader;
-            FragmentShader = fragmentShader;
-        }
-
-        public int Id
-        {
-            get { return id; }
-        }
-
-        public VertexShader VertexShader
-        {
-            get { return vertexShader; }
-            set
-            {
-                vertexShader = value;
-                GL.AttachShader(id, vertexShader.Id);
-                GL.LinkProgram(id);
-            }
-        }
-
-        public FragmentShader FragmentShader
-        {
-            get { return fragmentShader; }
-            set
-            {
-                fragmentShader = value;
-                GL.AttachShader(id, fragmentShader.Id);
-                GL.LinkProgram(id);
-            }
-        }
-
-        public void Use()
-        {
-            GL.UseProgram(id);
-        }
-    }
-
-    public class FragmentShader : Shader
-    {
-        public FragmentShader(string shaderSourceCode) : base(shaderSourceCode)
-        {
-            id = compileShader(ShaderType.FragmentShader, shaderSourceCode);
-        }
-    }
     public class Window : GameWindow
     {
+        public VoxelGrid voxelGrid;
         public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) :
             base(gameWindowSettings, nativeWindowSettings)
         {
         }
 
-        private float[] _vertices =
-        {
-            -0.9f, -0.9f, 0,
-             0.9f, -0.9f, 0,
-             0.9f,  0.9f, 0,
-             0.9f,  0.9f, 0,
-            -0.9f,  0.9f, 0,
-            -0.9f, -0.9f, 0
-        };
-
-        private int _vertexBufferObject;
-
-        private int _vertexArrayObject;
-
-        private ShaderProgram _program;
-
         protected override void OnLoad()
         {
             base.OnLoad();
 
+            voxelGrid = new VoxelGrid();
             GL.ClearColor(1, 1, 1, 1);
-
-            _vertexBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
-
-            _vertexArrayObject = GL.GenVertexArray();
-            GL.BindVertexArray(_vertexArrayObject);
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-            GL.EnableVertexAttribArray(0);
-
-            var vertexShader = new VertexShader(File.ReadAllText(@"Resources\Shaders\VertexShader.glsl"));
-
-            var fragmentShader = new FragmentShader(File.ReadAllText(@"Resources\Shaders\FragmentShader.glsl"));
-
-            _program = new ShaderProgram(vertexShader, fragmentShader);
-            _program.Use();
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
@@ -148,9 +30,7 @@ namespace Render
             base.OnRenderFrame(e);
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
-            _program.Use();
-            GL.BindVertexArray(_vertexArrayObject);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+            voxelGrid.Draw(null);
 
             SwapBuffers();
         }
