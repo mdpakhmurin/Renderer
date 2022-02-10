62a0da11180d365d16d1190e5b2321069e0d694a
mdpakhmurin <mdpakhmurin@gmail.com>
2022-02-10 10:29:06 +0700

    Отрисовщик  и данные о 3х мерной таблице вокселей и вынесены в подклассы

--- DrawableObject.cs
+++ DrawableObject.cs
@@ -13,119 +13,167 @@ namespace Scene.Defaults
         virtual public void Draw(Camera camera) { }
     }
 
-    public class VoxelGrid : DrawableObject
+    public partial class VoxelGrid : DrawableObject
     {
         private ShaderProgram shaderProgram;
-        private int vbo;
-        private int vao;
-
-
-        private void initVBO()
-        {
-            // Координаты углов "экрана" отрисовки
-            //
-            // Экран - 3х мерная плоскость (состояющая из двух треугольников)
-            // на которой будет происходить отрисовка
-            // 
-            // Каждая строчка - координаты x,y соответствующего угла
-            // (координата z устанавливается в 0 в шейдере)
-            float[] canvasVerticesPos =
-            {
-                -0.9f, -0.9f,
-                 0.9f, -0.9f,
-                 0.9f,  0.9f,
-                 0.9f,  0.9f,
-                -0.9f,  0.9f,
-                -0.9f, -0.9f
-            };
-
-
-            vbo = GL.GenBuffer();
-
-            // Установка активного VBO для всей программы
-            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-
-            // Загрузка данных в VBO
-            GL.BufferData(
-                BufferTarget.ArrayBuffer,
-                canvasVerticesPos.Length * sizeof(float),
-                canvasVerticesPos,
-                BufferUsageHint.StaticDraw);
-        }
-
-        private void initVAO()
-        {
-            // Установка активного VBO
-            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-
-            vao = GL.GenVertexArray();
-
-            // Установка активного VAO
-            GL.BindVertexArray(vao);
-            
-            // Указывает активный массив атрибутов вершин
-            GL.EnableVertexAttribArray(0);
-            // Связывает активный VBO с активным индексом VAO
-            GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 0, 0);
-            GL.BindVertexArray(0);
-        }
-
-        private void initShaderProgram()
-        {
-            var vertexShader = new VertexShader(File.ReadAllText(@"Resources\Shaders\VertexShader.glsl"));
-            var fragmentShader = new FragmentShader(File.ReadAllText(@"Resources\Shaders\FragmentShader.glsl"));
-
-            shaderProgram = new ShaderProgram(vertexShader, fragmentShader);
-
-            var grid = new Grid(shaderProgram);
-            grid.Generate();
-        }
+        private Drawer drawer;
+        private Data data;
 
         public VoxelGrid()
         {
-            initVBO();
-            initVAO();
-            initShaderProgram();
+            drawer = new Drawer(this);
+            data = new Data(this);
+            data.GenerateGrid(new Vector3(3,3,3));
         }
 
         override public void Draw(Camera camera)
         {
             base.Draw(camera);
 
-            shaderProgram.Use();
-            GL.Uniform3(
-                shaderProgram.GetUniform("cameraPosition"),
-                camera.Transform.Position.GetAbsolutePosition());
-
-            GL.BindVertexArray(vao);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
-            GL.BindVertexArray(0);
+            drawer.Draw(camera);
         }
+
     }
 
-    public class Grid {
-        private ShaderProgram shaderProgram;
-
-        public Grid (ShaderProgram shaderProgram){
-            this.shaderProgram = shaderProgram;
-        }
-
-        public void Generate()
+    public partial class VoxelGrid : DrawableObject
+    {
+        private class Drawer
         {
-            shaderProgram.Use();
+            private VoxelGrid voxelGrid;
+            private int vbo;
+            private int vao;
 
-            int ssbo = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ShaderStorageBuffer, ssbo);
-
-            int[] voxels = new int[3 * 3 * 3 * 3];
-            for (int i = 0; i < voxels.Length; i++)
+            public Drawer(VoxelGrid voxelGrid)
             {
-                voxels[i] = 1;
+                this.voxelGrid = voxelGrid;
+                init();
             }
-            GL.BufferData(BufferTarget.ShaderStorageBuffer, sizeof(int) * voxels.Length, voxels, BufferUsageHint.DynamicDraw);
-            GL.BindBuffersBase(BufferRangeTarget.ShaderStorageBuffer, 3, 1, ref ssbo);
-            GL.BindBuffer(BufferTarget.ShaderStorageBuffer, 0);
-            Console.WriteLine(GL.GetError());
+
+            /// <summary>
+            /// Инициализирует VBO, занося в него данные
+            /// описывающие экран отрисовки.
+            /// </summary>
+            private void initVBO()
+            {
+                // Координаты углов "экрана" отрисовки
+                //
+                // Экран - 3х мерная плоскость (состояющая из двух треугольников)
+                // на которой будет происходить отрисовка
+                // 
+                // Каждая строчка - координаты x,y соответствующего угла
+                // (координата z устанавливается в 0 в шейдере)
+                float[] canvasVerticesPos =
+                {
+                    -0.9f, -0.9f,
+                     0.9f, -0.9f,
+                     0.9f,  0.9f,
+                     0.9f,  0.9f,
+                    -0.9f,  0.9f,
+                    -0.9f, -0.9f
+                };
+
+
+                vbo = GL.GenBuffer();
+
+                // Установка активного VBO для всей программы
+                GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+
+                // Загрузка данных из canvasVerticesPos в VBO
+                GL.BufferData(
+                    BufferTarget.ArrayBuffer,
+                    canvasVerticesPos.Length * sizeof(float),
+                    canvasVerticesPos,
+                    BufferUsageHint.StaticDraw);
+            }
+
+            /// <summary>
+            /// Инициализирует VAO, используя данные из VBO
+            /// </summary>
+            private void initVAO()
+            {
+                // Установка активного VBO
+                GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+
+                vao = GL.GenVertexArray();
+
+                // Установка активного VAO
+                GL.BindVertexArray(vao);
+
+                // Указывает активный массив атрибутов вершин
+                GL.EnableVertexAttribArray(0);
+                // Связывает активный VBO с активным индексом VAO
+                GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 0, 0);
+
+                GL.BindVertexArray(0);
+            }
+
+            /// <summary>
+            /// Загружает шейдеры и создает шейдерную программу
+            /// </summary>
+            private void initShaderProgram()
+            {
+                var vertexShader = new VertexShader(File.ReadAllText(@"Resources\Shaders\VertexShader.glsl"));
+                var fragmentShader = new FragmentShader(File.ReadAllText(@"Resources\Shaders\FragmentShader.glsl"));
+
+                voxelGrid.shaderProgram = new ShaderProgram(vertexShader, fragmentShader);
+            }
+
+            /// <summary>
+            /// Начальная инициализация отрисовщика
+            /// </summary>
+            private void init()
+            {
+                initVBO();
+                initVAO();
+                initShaderProgram();
+            }
+
+            public void Draw(Camera camera)
+            {
+                voxelGrid.shaderProgram.Use();
+
+                // Установка позиции камеры,
+                GL.Uniform3(
+                    voxelGrid.shaderProgram.GetUniform("cameraPosition"),
+                    camera.Transform.Position.GetAbsolutePosition());
+
+                // Использование VAO для отрисовки треугольников.
+                // (2 треугольника образующих прямоугольник экрана отрисовки).
+                GL.BindVertexArray(vao);
+                GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+                GL.BindVertexArray(0);
+            }
+        }
+    }
+    
+    public partial class VoxelGrid : DrawableObject
+    {
+        private class Data
+        {
+            private VoxelGrid voxelGrid;
+
+            public Data(VoxelGrid voxelGrid)
+            {
+                this.voxelGrid = voxelGrid;
+            }
+
+            public void GenerateGrid(Vector3 size)
+            {
+                voxelGrid.shaderProgram.Use();
+
+                int ssbo = GL.GenBuffer();
+                GL.BindBuffer(BufferTarget.ShaderStorageBuffer, ssbo);
+
+                int[] voxels = new int[3 * 3 * 3 * 3];
+                for (int i = 0; i < voxels.Length; i++)
+                {
+                    voxels[i] = 1;
+                }
+                GL.BufferData(BufferTarget.ShaderStorageBuffer, sizeof(int) * voxels.Length, voxels, BufferUsageHint.DynamicDraw);
+                GL.BindBuffersBase(BufferRangeTarget.ShaderStorageBuffer, 3, 1, ref ssbo);
+                GL.BindBuffer(BufferTarget.ShaderStorageBuffer, 0);
+                Console.WriteLine(GL.GetError());
+            }
         }
     }
 }
