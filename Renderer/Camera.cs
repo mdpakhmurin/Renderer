30aff6cd7bee56241fd53bccfa17274ced9314a1
mdpakhmurin <mdpakhmurin@gmail.com>
2022-02-16 20:35:32 +0700

    Изменения класса камеры

---
+++ Renderer/Camera.cs
@@ -0,0 +1,34 @@
+﻿using OpenTK.Mathematics;
+using Scene.SceneObject;
+
+namespace Scene.Defaults
+{
+    public class Camera : SceneObject.SceneObject
+    {
+        public enum CameraType
+        {
+            Orthographic,
+            Perspective
+        }
+
+        public Camera(string name = "newCamera") : base(name) {
+            Width = 10;
+            Height = 10;
+
+            Far = 1;
+            Near = 100;
+
+            Type = CameraType.Orthographic;
+        }
+
+        public float Width { get; set; }
+
+        public float Height { get; set; }
+
+        public float Near { get; set; }
+
+        public float Far { get; set; }
+
+        public CameraType Type { get; set; }
+    }
+}
